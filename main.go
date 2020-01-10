ba8f909d3b04276c11776390b2e183ce0e9cb121
higgyhiggy <hector3.mt@gmail.com>
2020-01-10 00:20:58 -0500

    updated read me with user stories

--- main.go
+++ main.go
@@ -1,42 +1,9 @@
 package main
 
 import (
-	"flag"
-	"math/rand"
-	"time"
+	"fmt"
 )
 
-var answers [8]string
-var abs bool
-
-func init() {
-	answers = [8]string{
-		"No",
-		"Yes",
-		"Maybe",
-		"Try Again",
-		"Pray",
-		"I don't know",
-		"It's unclear",
-		"Can you repeat the question?",
-	}
-
-	rand.Seed(time.Now().UnixNano())
-
-	flag.BoolVar(&abs, "abs", false, "absolute answers only")
-	flag.Parse()
-}
-
-func random() int {
-	var n int
-	if abs {
-		n = 2
-	} else {
-		n = len(answers)
-	}
-	return rand.Intn(n)
-}
-
 func main() {
-	println(answers[random()])
+	fmt.Println("this is going to be a note taking app")
 }
