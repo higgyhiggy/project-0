b68fb1f3e54e84768b6b5f8e49383b5eb370eb0c
higgyhiggy <hector3.mt@gmail.com>
2020-01-11 19:41:21 -0500

    modified print statement

--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	fmt.Println("sho rururururururur")
 	//asks the env to get user name
 	user := os.Getenv("USER")
-	fmt.Println(file.Create(file, "day 1"))
+	fmt.Println(file.Create(file, "day 1"), user)
 
 }
