07e1c3b1f5f927bc7e11839db1ba3c58b4c19af4
higgyhiggy <hector3.mt@gmail.com>
2020-01-14 16:14:00 -0500

    refactor

--- file/file.go
+++ file/file.go
@@ -1,17 +1,26 @@
 // Package file is an file.go libary
 package file
 
+import (
+	"os"
+	"time"
+)
+
 //Create is a function that takes in a string and  creates a file or opens a file
 // but also appending the  date it was create jan 8 , 2020 : "filename"
-func Create(name bool, fname string) string {
+func Create(name bool) string {
 	var n string
 
 	if name {
 
-		n = fname
+		if len(os.Args) < 3 {
+			n = time.Now().Format("jan-02-2006") + " " + time.Now().Format("03:04:05 PM") + ": " + "quicknotes"
+		} else {
+			n = time.Now().Format("jan-02-2006") + " " + time.Now().Format("03:04:05 PM") + ": " + os.Args[2]
+		}
 
 	} else {
-
+		n = time.Now().Format("jan-02-2006") + " " + time.Now().Format("03:04:05 PM") + ": " + "quicknotes"
 	}
 	return n
 }
