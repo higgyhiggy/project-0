fdc335ee340748fe1601f1e0972762d659961a51
higgyhiggy <hector3.mt@gmail.com>
2020-01-19 12:57:58 -0500

    REFactor

--- file/file.go
+++ file/file.go
@@ -12,9 +12,10 @@ import (
 // and if the flag is missing also
 func Create(name bool) string {
 	var n string
-
+	//here we see flag to set name is active to to choose a specifc name if not a random one is general one is given
 	if name {
 		fmt.Println(len(os.Args))
+		//these condition statement are to handle the erros if they put the flag but dont put a name
 		if len(os.Args) >= 4 {
 
 			n = time.Now().Format(os.Args[3]+"/jan-02-2006") + " " + time.Now().Format("03:04:05 PM") + ": " + os.Args[2]
@@ -36,5 +37,6 @@ func Create(name bool) string {
 	//return n
 
 	//fmt.Println(n)
+	// n the name the file will have
 	return n
 }
--- xsnotesd/xsnotesd.go
+++ xsnotesd/xsnotesd.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+
+	"github.com/higgyhiggy/project-0/config"
+	_ "github.com/higgyhiggy/project-0/config"
 )
 
-func main() {
-	println("Server is running on port 8080")
+var name string
 
+func main() {
+
+	fmt.Println("Server is running on port 8080")
+	//takes a patter string for the handle and takes in a handler
+	//then handler returns the content of a file system
 	http.Handle("/", http.FileServer(http.Dir("web")))
+	//deals with the handl with the anonymous handler function
 	http.HandleFunc("/8ball", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "higgyhiggy time")
+		//r is a request to url
+		//we are working with form to be able to use user input
+		//if no name is given then fall back valur will be anonymous
+		name = r.FormValue("name")
+		if name == "" {
+			name = "anonymous"
+		}
+		fmt.Fprint(w, "<h1>"+config.Word[rand.Intn(len(config.Word))].Text+"</h1>", " ", name)
 	})
-	http.ListenAndServe(":8080", nil)
+
+	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+	// can make some infinite loop to add some other functionality
+	go http.ListenAndServe(":8080", nil)
+
+	time.Sleep(30 * time.Second)
+
 }
